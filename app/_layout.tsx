71e4e67c0f7f74afe9cf448ccae4da7a187145e1
JerryLegend254 <jerryokuto713@gmail.com>
2024-06-13 12:05:13 +0300

    added supabase and eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,8 @@
-import { Drawer } from "expo-router/drawer";
-import { Ionicons } from "@expo/vector-icons";
-import CustomDrawerComponent from "@/components/CustomDrawerComponent";
-export default function App() {
+import { Stack } from "expo-router";
+export default function Layout() {
   return (
-    <Drawer
-      screenOptions={{
-        drawerStyle: { backgroundColor: "#6759FF" },
-        drawerLabelStyle: { marginLeft: -20 },
-        drawerActiveBackgroundColor: "#fff",
-        drawerActiveTintColor: "#6759FF",
-        drawerInactiveTintColor: "#fff",
-        drawerItemStyle: { marginHorizontal: 20 },
-      }}
-      drawerContent={CustomDrawerComponent}
-    >
-      <Drawer.Screen
-        name="(home)"
-        options={{
-          drawerLabel: "Home",
-          title: "Home",
-          drawerIcon: ({ size, color }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
-          ),
-        }}
-      />
-    </Drawer>
+    <Stack initialRouteName="index" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
