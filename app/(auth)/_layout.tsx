bf0a1b7cd622d5596addd1f5b9844de7aaf554e6
JerryLegend254 <jerryokuto713@gmail.com>
2024-06-28 12:51:33 +0300

    code cleanup and using sessions instead of user object

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,8 +2,8 @@ import { useAuth } from "@/providers/AuthProvider";
 import { Redirect, Stack } from "expo-router";
 
 export default function AuthLayout() {
-  const user = useAuth();
-  if (user.user) {
+  const { user } = useAuth();
+  if (user?.user) {
     return <Redirect href="(home)" />;
   } else {
     return <Stack screenOptions={{ headerShown: false }} />;
